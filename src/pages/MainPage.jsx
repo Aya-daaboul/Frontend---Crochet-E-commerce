37781a87c262542da9535a796e69a4f08e32d695
Aya-daabou <Ayadaaboul02@outlook.com>
2025-05-26 23:01:50 +0300

    components now hid on main pagee wheen logged in

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
 import "../css/main.css";
 import Banner from "../components/Banner.jsx";
 import Reasons from "../components/Reasons.jsx";
 import ServicesSec from "../components/ServicesSec.jsx";
 import ProductsSec from "../components/ProductsSec.jsx";
 import CraftSec from "../components/CraftSec.jsx";
+import Header from "../components/header.jsx";
+import Footer from "../components/footer.jsx";
 
 const MainPage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    setIsLoggedIn(!!user);
+  }, []);
+
   return (
     <>
       <Banner />
-      <Reasons />
-      <ServicesSec />
-      <ProductsSec />
-      <CraftSec />
+
+      {/* Only show when not logged in */}
+      {!isLoggedIn && (
+        <>
+          <Reasons />
+          <ServicesSec />
+          <ProductsSec />
+          <CraftSec />
+        </>
+      )}
     </>
   );
 };
